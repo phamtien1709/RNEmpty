7621bbbaa4e959b5b2d4512bad5e1faf065ae3bf
Pham Tien <phamtien1709@gmail.com>
2019-01-02 11:41:06 +0700

    layout and button

--- EmptyRN/EmptyWeshare/layout/Body.js
+++ EmptyRN/EmptyWeshare/layout/Body.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, AppRegistry, View } from 'react-native';
+import { StyleSheet, AppRegistry, View, Button, Alert } from 'react-native';
+import PizzaTranslator from '../components/PizzaTrans';
+import ButtonBase from '../components/ButtonBase';
 
 export default class Body extends Component {
     render() {
         return (
-            <View style={{ flex: 8, backgroundColor: 'skyblue' }} />
+            <View style={{ flex: 8, backgroundColor: 'skyblue' }}>
+                <PizzaTranslator></PizzaTranslator>
+                <ButtonBase></ButtonBase>
+            </View>
         )
     }
 }
--- EmptyRN/EmptyWeshare/layout/Footer.js
+++ EmptyRN/EmptyWeshare/layout/Footer.js
@@ -5,9 +5,9 @@ export default class Footer extends Component {
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: 'steelblue', flexDirection: 'row' }}>
-                <View style={{ width: Dimensions.get('window').width / 3, height: 50, backgroundColor: 'salmon' }} />
-                <View style={{ width: Dimensions.get('window').width / 3, height: 50, backgroundColor: 'gold' }} />
-                <View style={{ width: Dimensions.get('window').width / 3, height: 50, backgroundColor: 'tomato' }} />
+                <View style={{ width: Dimensions.get('window').width / 3, flex: 1, backgroundColor: 'salmon' }} />
+                <View style={{ width: Dimensions.get('window').width / 3, flex: 1, backgroundColor: 'gold' }} />
+                <View style={{ width: Dimensions.get('window').width / 3, flex: 1, backgroundColor: 'tomato' }} />
             </View>
         )
     }
